a5c20073b529b1bcff976cbdb066e39aa7544709
VladislavDeveloper <83352504+VladislavDeveloper@users.noreply.github.com>
2021-10-04 19:09:32 +0300

    main-page-style
    Created style for main user page

---
+++ public/componentsJS/main-info.js
@@ -0,0 +1,17 @@
+Vue.component('main-info', {
+    template: ` 
+    <div class="brand">
+                <div class="brandInside">
+                    <div class="left_brand">
+                        <img src="img/macbook_logo_or.jpg" alt="">
+                    </div>
+                    <div class="right_brand">
+                        <div class="brandText">
+                            <div>APPLE</div>
+                            <div>official retailer<span> Apple </span></div>
+                    </div>
+                </div>
+            </div>
+    </div>    
+    `
+})
